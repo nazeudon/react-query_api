f897998350e255f8c70a2b89a44696c2139923f2
nazeudon <nazeudon48112@icloud.com>
2021-06-04 10:00:00 +0900

    added views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Task, Tag
+from .serializers import TaskSerializer, TagSerializer
 
-# Create your views here.
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
